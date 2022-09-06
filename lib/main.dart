23efee0a1aa917ad38b8912d046e58a33e1c9e25
nethunter137 <nethunter137@gmail.com>
2022-09-05 23:17:58 -0400

    step2

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes2/firebase_options.dart';
-import 'package:mynotes2/views/login.dart';
 import 'package:mynotes2/views/login_view.dart';
 import 'package:mynotes2/views/register_view.dart';
 
@@ -45,32 +44,6 @@ class Homepage extends StatelessWidget {
             return const Text("Loading...");
         }
       },
-    );
-  }
-}
-
-class VerifyEmailView extends StatefulWidget {
-  const VerifyEmailView({Key? key}) : super(key: key);
-
-  @override
-  State<VerifyEmailView> createState() => _VerifyEmailViewState();
-}
-
-class _VerifyEmailViewState extends State<VerifyEmailView> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Login")),
-      body: Column(children: [
-        const Text("Please verify your email address:"),
-        TextButton(
-          onPressed: () async {
-            final user = FirebaseAuth.instance.currentUser;
-            await user?.sendEmailVerification();
-          },
-          child: const Text("Send email verification"),
-        )
-      ]),
     );
   }
 }
